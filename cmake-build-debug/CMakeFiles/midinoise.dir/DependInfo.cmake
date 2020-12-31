a0a01d7924e48d174efa0a6f5bf1055bb7bb637b
Robert McKay <robert@mckay.com>
2020-12-31 19:19:01 +0000

    added other files

---
+++ cmake-build-debug/CMakeFiles/midinoise.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rm/IdeaProjects/midinoise/keyboard.c" "/home/rm/IdeaProjects/midinoise/cmake-build-debug/CMakeFiles/midinoise.dir/keyboard.c.o"
+  "/home/rm/IdeaProjects/midinoise/loadmid.c" "/home/rm/IdeaProjects/midinoise/cmake-build-debug/CMakeFiles/midinoise.dir/loadmid.c.o"
+  "/home/rm/IdeaProjects/midinoise/notes.c" "/home/rm/IdeaProjects/midinoise/cmake-build-debug/CMakeFiles/midinoise.dir/notes.c.o"
+  "/home/rm/IdeaProjects/midinoise/parse2.c" "/home/rm/IdeaProjects/midinoise/cmake-build-debug/CMakeFiles/midinoise.dir/parse2.c.o"
+  "/home/rm/IdeaProjects/midinoise/piano.c" "/home/rm/IdeaProjects/midinoise/cmake-build-debug/CMakeFiles/midinoise.dir/piano.c.o"
+  "/home/rm/IdeaProjects/midinoise/play.c" "/home/rm/IdeaProjects/midinoise/cmake-build-debug/CMakeFiles/midinoise.dir/play.c.o"
+  "/home/rm/IdeaProjects/midinoise/play2.c" "/home/rm/IdeaProjects/midinoise/cmake-build-debug/CMakeFiles/midinoise.dir/play2.c.o"
+  "/home/rm/IdeaProjects/midinoise/popen.c" "/home/rm/IdeaProjects/midinoise/cmake-build-debug/CMakeFiles/midinoise.dir/popen.c.o"
+  "/home/rm/IdeaProjects/midinoise/xclient.c" "/home/rm/IdeaProjects/midinoise/cmake-build-debug/CMakeFiles/midinoise.dir/xclient.c.o"
+  "/home/rm/IdeaProjects/midinoise/xkeypress.c" "/home/rm/IdeaProjects/midinoise/cmake-build-debug/CMakeFiles/midinoise.dir/xkeypress.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/midinoise.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/midinoise.dir/keyboard.c.o"
+  "CMakeFiles/midinoise.dir/loadmid.c.o"
+  "CMakeFiles/midinoise.dir/notes.c.o"
+  "CMakeFiles/midinoise.dir/parse2.c.o"
+  "CMakeFiles/midinoise.dir/piano.c.o"
+  "CMakeFiles/midinoise.dir/play.c.o"
+  "CMakeFiles/midinoise.dir/play2.c.o"
+  "CMakeFiles/midinoise.dir/popen.c.o"
+  "CMakeFiles/midinoise.dir/xclient.c.o"
+  "CMakeFiles/midinoise.dir/xkeypress.c.o"
+  "midinoise.pdb"
+  "midinoise"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/midinoise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
