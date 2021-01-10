707f8ee18b80a5fed0283ae92ac7e30d5f625903
Robert McKay <robert@mckay.com>
2021-01-10 04:52:34 +0000

    changes to instrument

--- play2.c
+++ play2.c
@@ -30,6 +30,49 @@ int init_sound()
 
 }
 
+float pure_tone(float time, float freq_hz) {
+    return sin(time * freq_hz * 2 / 44100);
+}
+
+// https://en.wikipedia.org/wiki/Phase_modulation
+float modulated_tone(float time, float freq_hz, float mod_hz, float amt) {
+    return sin((time * freq_hz * 2 / 44100) + (amt * M_PI/2 * sin(mod_hz * time) ));
+}
+
+
+//int play_note_fraction(double note, int fraction) {
+//
+//    unsigned short bbuf[44100];
+//    signed short min = 32768;
+//    signed short max = 0;
+//    float decay = 1;
+//    for (float i=0;i<44100/2;i+=1) {
+//        float fsample;
+//        if (i<3000) {
+////    fsample = pure_tone(i, note) * i * 5;
+//            fsample = modulated_tone(i, note, 300, i/3000) * i * 5;
+//        } else {
+////    fsample = pure_tone(i, note) * 32768;
+//            fsample = modulated_tone(i, note, 300, decay) * 32768;
+//            decay-=0.00002;
+//        }
+//
+//        signed short sample = (signed short)(fsample - 32768);
+//
+//        // convert sine wave to square wave
+//        // sample = ((((sample > 0) - (sample < 0 ))+1) * 65530) - 32768;
+//
+//        if (sample>max) { max = sample; }
+//        if (sample<min) { min = sample; }
+//
+//        bbuf[(int)i]=sample;
+//
+//    }
+//    printf("min: %d, max: %d\n", min, max);
+//    write(dsp, &bbuf, sizeof(bbuf)/2);
+//
+//}
+
 int play_note(double note)
 {
   play_note_fraction(note, 1000);
@@ -49,6 +92,7 @@ int play_notes(char playing[]) {
   int c=0;
   char by;
   int y=0;
+  float fsample;
   //int fraction = 100; // just cause
 
   int first=1;
@@ -57,16 +101,27 @@ int play_notes(char playing[]) {
     if (np!=0) {
       double freq = notes[i];
       double rep = (rate / freq);
+      float decay = 1;
       y = 0;
       for (x=0;x<65536;x+=(65536.0/rep))
       {
-        if (first) {
-          buf[y]=sin(freq * x) * 256;
-          first=0;
-        } else {
-          buf[y] = (buf[y] + (sin(freq * x) * 256)) / 2;
-        }
-        y++;
+
+          if (x<3000) {
+              fsample = modulated_tone(x, freq, 300, i / 3000) * i * 5;
+          } else {
+              fsample = modulated_tone(x, freq, 300, decay) * 32768;
+              decay-=0.00002;
+          }
+          //fsample = pure_tone(x, freq) * 256;
+          if (first) {
+              //buf[y]=sin(freq * x) * 256;
+              buf[y]=fsample;
+              first=0;
+          } else {
+              //buf[y] = (buf[y] + (sin(freq * x) * 256)) / 2;
+              buf[y] = ((buf[y] + fsample) / 2);
+          }
+          y++;
       }
     }
 
